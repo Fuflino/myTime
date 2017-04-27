187920fef065af5ba4196dce964f844f7aa2c090
Stefan O <input.goes.here@gmail.com>
2017-04-27 11:52:58 +0200

    add and substract hours in GUI

--- src/mytime/gui/controller/VolunteerMainViewController.java
+++ src/mytime/gui/controller/VolunteerMainViewController.java
@@ -5,7 +5,6 @@
  */
 package mytime.gui.controller;
 
-import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXMasonryPane;
 import java.io.IOException;
 import java.net.URL;
@@ -17,6 +16,7 @@ import javafx.animation.Interpolator;
 import javafx.animation.KeyFrame;
 import javafx.animation.KeyValue;
 import javafx.animation.Timeline;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -26,9 +26,9 @@ import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.shape.Circle;
+import javafx.scene.input.MouseEvent;
 import javafx.util.Duration;
-import mytime.be.Volunteer;
+import mytime.gui.model.VolunteerModel;
 
 /**
  * FXML Controller class
@@ -39,59 +39,52 @@ public class VolunteerMainViewController implements Initializable
 {
 
     @FXML
-    private JFXButton btnBack;
-    @FXML
-    private ImageView imageView;
-    @FXML
-    private Label lblName;
-    @FXML
-    private Label lblEmail;
-    @FXML
-    private Label lblPhonenumber;
-    @FXML
     private JFXMasonryPane masonryPane;
     @FXML
     private ScrollPane scrollPane;
+    
+    private VolunteerModel volunteerModel;
+    @FXML
+    private Label lblUserHourInput;
 
     /**
      * Initializes the controller class.
      */
     @Override
-   public void initialize(URL url, ResourceBundle rb)
+    public void initialize(URL url, ResourceBundle rb)
     {
+        volunteerModel = VolunteerModel.getInstance();
+        lblUserHourInput.textProperty().bind(volunteerModel.getUserHourInput().asString());
         ArrayList<Node> elements = new ArrayList<>();
         masonryPane.setCellHeight(100);
         masonryPane.getStyleClass().add("defaultBackgroundColor");
-        
-        
-        
-        
 
         try
         {
             for (int i = 0; i < 5; i++)
             {
                 elements.add(getNodeForGuild("Vikinge Laug"));
-                
+
             }
         } catch (IOException ex)
         {
             Logger.getLogger(LoginMainViewController.class.getName()).log(Level.SEVERE, null, ex);
         }
 
-        
         masonryPane.getChildren().setAll(elements);
-    }   
-    
+    }
+
     /**
-     * Method for getting a guild as a node, so that we can load it in our view as a JAVAFX component.
+     * Method for getting a guild as a node, so that we can load it in our view
+     * as a JAVAFX component.
+     *
      * @param volunteer
      * @return
-     * @throws IOException 
+     * @throws IOException
      */
     private Node getNodeForGuild(String guildName) throws IOException
     {
-         FXMLLoader loader = new FXMLLoader(getClass().getResource("/mytime/gui/view/VolunteerOneGuild.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/mytime/gui/view/VolunteerOneGuild.fxml"));
         Node node = loader.load();
         Button button = (Button) node;
         // load the image
@@ -115,13 +108,10 @@ public class VolunteerMainViewController implements Initializable
         button.setPrefWidth(150);
         button.getStyleClass().add("LoginVolunteerBtn");
 
-        
-
         button.setScaleX(0);
         button.setScaleY(0);
         button.setGraphic(iv1);
         button.setText(guildName);
-        
 
         Timeline animation = new Timeline(new KeyFrame(Duration.millis(240), new KeyValue(button.scaleXProperty(), 1, Interpolator.EASE_BOTH),
                 new KeyValue(button.scaleYProperty(), 1, Interpolator.EASE_BOTH)));
@@ -129,6 +119,34 @@ public class VolunteerMainViewController implements Initializable
         animation.play();
 
         return node;
-    }   
-    
+    }
+    /**
+     * Gets called when you substract a hour
+     * @param event 
+     */
+    @FXML
+    private void handleHourInputDown(ActionEvent event)
+    {
+        volunteerModel.minusOneUpInUserHoursInput();
+    }
+    /**
+     * Gets called when you add a hour up
+     * @param event 
+     */
+    @FXML
+    private void handleHourInputUp(ActionEvent event)
+    {
+        volunteerModel.addOneUpInUserHourInput();
+    }
+    /**
+     * Gets called when you press on the execute button.
+     * @param event 
+     */
+    @FXML
+    private void handleExecuteHourInput(ActionEvent event)
+    {
+        volunteerModel.executeHourDocumentation();
+    }
+
+
 }
--- src/mytime/gui/model/VolunteerModel.java
+++ src/mytime/gui/model/VolunteerModel.java
@@ -5,11 +5,77 @@
  */
 package mytime.gui.model;
 
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+
 /**
  *
  * @author Stefan-VpcEB3J1E
  */
 public class VolunteerModel
 {
-    
+
+    private static VolunteerModel INSTANCE;
+    private IntegerProperty userHourInput;
+
+    /**
+     * Part of the singleton pattern
+     */
+    private VolunteerModel()
+    {
+        userHourInput = new SimpleIntegerProperty(0);
+    }
+
+    /**
+     * A part of the singleton pattern
+     *
+     * @return An instance of a VolunteerModel
+     */
+    public static VolunteerModel getInstance()
+    {
+        if (INSTANCE == null)
+        {
+            INSTANCE = new VolunteerModel();
+        }
+        return INSTANCE;
+    }
+    /**
+     * 
+     * @return The IntegerProperty of the hours that the user have added in the GUI
+     */
+    public IntegerProperty getUserHourInput()
+    {
+        return userHourInput;
+    }
+    /**
+     * Adds one up in user hour input
+     */
+    public void addOneUpInUserHourInput()
+    {
+        userHourInput.set(userHourInput.get() + 1);
+    }
+    /**
+     * Substracts one from the user hour input
+     */
+    public void minusOneUpInUserHoursInput()
+    {
+        if (!userHourInput.isEqualTo(0).get())
+        {
+            userHourInput.set(userHourInput.get() - 1);
+        }
+    }
+    /**
+     * Documents the hours into the database
+     * 
+     */
+    public void executeHourDocumentation()
+    {
+        int hoursToDocumentate = userHourInput.get();
+        userHourInput.set(0);
+        
+        
+        
+        
+    }
+
 }
