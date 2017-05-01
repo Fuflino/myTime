369c98ed027e09fc8babdfbe85bb63986058f1d4
mashtoe <mashtoe@hotmail.com>
2017-05-01 13:57:33 +0200

    Back button goes back, but doesnt update

--- src/mytime/gui/controller/VolunteerTopViewController.java
+++ src/mytime/gui/controller/VolunteerTopViewController.java
@@ -5,9 +5,22 @@
  */
 package mytime.gui.controller;
 
+import com.jfoenix.controls.JFXButton;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+import mytime.gui.model.VolunteerModel;
 
 /**
  * FXML Controller class
@@ -17,6 +30,17 @@ import javafx.fxml.Initializable;
 public class VolunteerTopViewController implements Initializable
 {
 
+    @FXML
+    private ImageView imageView;
+    @FXML
+    private Label lblName;
+    @FXML
+    private Label lblEmail;
+    @FXML
+    private Label lblPhonenumber;
+    @FXML
+    private JFXButton btnBack;
+
     /**
      * Initializes the controller class.
      */
@@ -25,5 +49,37 @@ public class VolunteerTopViewController implements Initializable
     {
         // TODO
     }    
+
+    /**
+     * Closes this view and loads the mainview with all the volunteers at the
+     * current location.
+     * @param event 
+     */
+    @FXML
+    private void handleGoBack(ActionEvent event)
+    { 
+        VolunteerModel vmodel = VolunteerModel.getInstance();
+        vmodel.getUserHourInput().set(0);
+        vmodel.setCurrentGuild(null);
+        vmodel.setCurrentVolunteer(null);
+        
+        Stage mainView = (Stage) btnBack.getScene().getWindow();
+        mainView.close();
+
+        Parent mainViewLoad = null;
+        try
+        {
+            mainViewLoad = FXMLLoader.load(getClass().getResource("/mytime/gui/view/LoginMainView.fxml"));
+        } catch (IOException ex)
+        {
+            Logger.getLogger(LoginOneVolunteerController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        Scene scene = new Scene(mainViewLoad);
+
+        mainView.setScene(scene);
+        mainView.setResizable(true);
+        mainView.show();
+    }
     
 }
